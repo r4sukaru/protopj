525531047b2e7b60bf9b900db4725e2e47b267ea
Sho Ochiai <Ochiai@example.com>
2019-07-12 18:18:22 +0900

    20190712_commit_last
    view.py_107_shousai = GoodsTBL.objects.select_related().filter(exact_goodsid & exact_productno & exact_deleteflag)

--- searchapp/views.py
+++ searchapp/views.py
@@ -20,92 +20,6 @@ from django.shortcuts import render, get_object_or_404
     # (ListViewの場合、何も設定しないとhtml名の最後に[_list]が付く)
     #template_name = 'searchapp/search.html'
 
-    #def post(self, request, *args, **kwargs):
-        #"""
-       # 検索フォームに入力された値をセッションに格納するメソッド
-       # ⇒初期アクセスでは、検索フォームに入力されていない⇒セッションに格納される処理は呼ばれない
-        #⇒初期アクセスではこのメソッドは呼ばれない
-        #"""
-        # 検索値を格納するリストを新規作成
-        #form_value = [
-                #self.request.POST.get('title', None),
-                #self.request.POST.get('category', None),
-                #self.request.POST.get('price', None),
-            #]
-
-        # 検索値を格納するリストをセッションで管理する
-        #request.session['form_value'] = form_value
-
-        #'''
-        #self.request.GET = self.request.GET.copy()
-        #self.request.GET.clear()
-        #'''
-
-        # generic/list.pyのget()メソッドが呼び出される
-        #return self.get(request, *args, **kwargs)
-
-    #def get_context_data(self, **kwargs):
-        #"""
-         #初期値に空白を設定したテンプレートを返すメソッド
-         #⇒最初にサイトを呼び出すときに必ず呼ばれる
-        #"""
-        # 親クラスのメソッド呼び出し、変数contextに格納
-        #context = super().get_context_data(**kwargs)
-
-        #title = ''
-        #category = ''
-        #price = ''
-
-        # 最初はセッションに値が無いからこのif節は呼ばれない
-        #if 'form_value' in self.request.session:
-            #form_value = self.request.session['form_value']
-            #title = form_value[0]
-            #category = form_value[1]
-            #price = form_value[2]
-
-        # 辞書新規作成⇒初期値ではそれぞれ「空白」が設定
-        #default_data = {'title' :title, 'category' :category, 'price' :price}
-
-        # 入力フォームに初期値では空白を設定する処理
-        #test_form = GoodSearchForm(initial = default_data)
-
-        # 入力フォームに空白を指定したテンプレートを呼び出し、返却する処理
-        #context['test_form'] = test_form
-        #return context
-
-    #def get_queryset(self): # 呼び出された（オーバーライドされたメソッド）
-        #'''
-        #DBから検索条件に一致したデータを取得
-       # '''
-        # セッションに値があるときに動作する
-        # ⇒最初にページに入ったときはセッションに値がないので、下のelse文が実行される
-       # if 'form_value' in self.request.session:
-           # form_value = self.request.session['form_value']
-            #title = form_value[0]
-            #category = form_value[1]
-            #price = form_value[2]
-
-            #Qオブジェクトを各変数にインスタンス化
-            #condition_title = Q()
-            #condition_category = Q()
-            #condition_price = Q()
-
-            # クエリを発行
-            # 入力フォームに値が入っているかの判定
-            # 変数の長さが1以上で、null値ではない場合、クエリを発行する。
-            #if len(title) != 0 and title[0]:
-                #condition_title = Q(title__contains = title)
-            #if len(category) != 0 and category[0]:
-                #condition_category = Q(category__contains = category)
-           # if len(price) != 0 and price[0]:
-               # condition_price = Q(price__contains = price)
-
-            # 定義されたクエリを発行し、データをobject_listへ格納する。
-            #return GoodsTBL.objects.select_related().all()
-
-        #else:
-            #return GoodsTBL.objects.none() # 何も返さない
-
 
 class ResultList(generic.ListView):
     # modelは取り扱うモデルクラス(モデル名と紐づけ)
@@ -133,7 +47,7 @@ class ResultList(generic.ListView):
             # 親クラスのメソッド呼び出し、変数contextに格納
             context = super().get_context_data(**kwargs)
 
-            goodsid = 'AABBCC001S003'
+            goodsid = 'ZZYYXX001S003'
             productno = goodsid[:9]
             deleteflag = 0 # 有効状態
 
@@ -155,7 +69,7 @@ class ResultList(generic.ListView):
 
             # Qオブジェクトで定義した検索条件でクエリを発行する。
             goodsdetail = GoodsTBL.objects.select_related().filter(exact_goodsid & exact_productno)
-            pdfull = GoodsTBL.objects.select_related().filter(exact_productno)
+            #pdfull = GoodsTBL.objects.select_related().filter(exact_productno)
 
             # contextにクエリ発行した結果を追加し、テンプレートタグで使用可能にする。
             context['goods_form'] = goodsdetail
@@ -167,7 +81,7 @@ class ResultList(generic.ListView):
             '''
             詳細画面に表示する商品を検索する。
             '''
-            goodsid = 'AABBCC001S003'
+            goodsid = 'ZZYYXX001S003'
             productno = goodsid[:9]
             deleteflag = 0 # 有効状態
             '''
@@ -191,7 +105,6 @@ class ResultList(generic.ListView):
             exact_deleteflag = Q(deleteflag__exact = deleteflag) # 条件：論理削除フラグ = 1
 
             shousai = GoodsTBL.objects.select_related().filter(exact_goodsid & exact_productno & exact_deleteflag)
-            #print(GoodsTBL.objects.select_related().filter(exact_goodsid & exact_productno & exact_deleteflag)
             # 定義されたクエリを発行し、データをgoodsdetailsへ格納する。
             return shousai
 
